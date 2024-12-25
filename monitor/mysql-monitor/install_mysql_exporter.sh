53453c78460eb4ddc3ef995b55d140909cc8e400
zhoucy <zhoucy@tydic.com>
2024-12-25 10:52:54 +0800

    1

--- monitor/mysql-monitor/install_mysql_exporter.sh
+++ monitor/mysql-monitor/install_mysql_exporter.sh
@@ -23,14 +23,18 @@ do
   username=$(echo $line | awk '{print $4}')
   password=$(echo $line | awk '{print $5}')
   url="$ip:$port"
+  # 创建secret，内容包含mysql的用户名密码以及url
   kubectl create secret generic $mysql_instance-mysql-secret --from-literal=url=$url --from-literal=username=$username --from-literal=password=$password -n monitoring
+  # 创建mysql-exporter
   sed "s/\${mysql-instance}/$mysql_instance/g" yaml/mysql-exporter-deployment.yaml | kubectl apply -f -
+  # 创建mysql服务
   sed "s/\${mysql-instance}/$mysql_instance/g" yaml/mysql-exporter-service.yaml | kubectl apply -f -
-  #kubectl apply -f yaml/mysql-exporter-serviceMonitor.yaml
+  # 调用generate_serviceMonitor函数，生成serviceMontor的endpoint内容写入到临时文件 mysql-serviceMonitor_tmp.yaml
   generate_serviceMonitor $mysql_instance $url >> mysql-serviceMonitor_tmp.yaml
 done
-#注入serviceMontor对象
+#将生成的endpoint内容插入到yaml/mysql-exporter-serviceMonitor.yaml文件的第10行之后
 sed '10r mysql-serviceMonitor_tmp.yaml' yaml/mysql-exporter-serviceMonitor.yaml | kubectl apply -f -
+# 删除临时文件mysql-serviceMonitor_tmp.yaml
 if [ $? -eq 0 ];then
   rm -f mysql-serviceMonitor_tmp.yaml
 fi
